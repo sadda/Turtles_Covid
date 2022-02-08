111e11c4c1bc0bba84f2530abdba2f5254055cf0
sadda <lukas.adam.cr@gmail.com>
2022-02-08 16:46:45 +0100

    Confidence intervals computed

--- utilities.py
+++ utilities.py
@@ -26,23 +26,71 @@ def aggregate(x, step):
     return x_aggr, t_aggr
 
 
-def plot(x1, x2, n_aggr, return_fig=True):
-    x1_aggr, t_aggr = aggregate(x1, n_aggr)
-    x2_aggr, _ = aggregate(x2, n_aggr)
+def compute_confidence(x1, x2, k=1.96, c_max=10, p_ratio=1):
+    c0 = np.zeros(len(x1))
+    c1 = np.zeros(len(x1))
+    c2 = np.zeros(len(x1))
+    for (i, (x1_i, x2_i)) in enumerate(zip(x1, x2)):
+        if x1_i == 0 or x2_i == 0:
+            if x1_i == 0 and x2_i == 0:
+                c0_i = np.nan
+            elif x1_i == 0:
+                c0_i = c_max
+            else:
+                c0_i = 0
+            c1_i = 0
+            c2_i = c_max
+        else:            
+            c0_i = x2_i / x1_i / p_ratio
+            c1_i = x2_i / x1_i / p_ratio * np.exp(-k * np.sqrt(1/x1_i + 1/x2_i))
+            c2_i = x2_i / x1_i / p_ratio * np.exp(k * np.sqrt(1/x1_i + 1/x2_i))
+        c0[i] = c0_i
+        c1[i] = c1_i
+        c2[i] = c2_i
+    return c0, c1, c2
 
-    k = 1
-    c0 = x1_aggr / x2_aggr
-    c1 = x1_aggr / x2_aggr * np.exp(k * (1/x1_aggr + 1/x2_aggr))
-    c2 = x1_aggr / x2_aggr * np.exp(-k * (1/x1_aggr + 1/x2_aggr))
 
+def prepare_data(entries, years, year1, year2, n_aggr, instagram=None, **kwargs):
+    x1 = entries[years == year1]
+    x2 = entries[years == year2]
+
+    x1, t = aggregate(x1, n_aggr)
+    x2, _ = aggregate(x2, n_aggr)
+
+    if not instagram is None:
+        p1 = instagram.users.values[instagram.year == year1][0]
+        p2 = instagram.users.values[instagram.year == year1][0]
+        c0, c1, c2 = compute_confidence(x1, x2, p_ratio=p1/p2, **kwargs)
+    else:
+        c0, c1, c2 = compute_confidence(x1, x2, p_ratio=1, **kwargs)
+
+    return c0, c1, c2, t
+
+
+def plot_confidence(data, year1, year2, n_aggr, instagram, place=None, **kwargs):
+    if place is None:
+        entries = data.total.values
+    elif place == "boat":
+        entries = data.boat.values
+    elif place == "underwater":
+        entries = data.underwater.values
+    else:
+        raise(Exception('place should be None, boat or underwater'))
+    years = data.year.values
+    c0, c1, c2, t = prepare_data(entries, years, year1, year2, n_aggr, instagram, **kwargs)
+    
+    return plot_confidence0(c0, c1, c2, t, n_aggr, return_fig=False, **kwargs)
+
+
+def plot_confidence0(c0, c1, c2, t, n_aggr, return_fig=True, **kwargs):
     fig = plt.figure(facecolor=(1, 1, 1))
     ax = fig.add_subplot(111)
-    plt.plot(t_aggr, c0)
-    plt.plot(t_aggr, c1, color="grey", alpha=0.3)
-    plt.plot(t_aggr, c2, color="grey", alpha=0.3)
-    plt.fill_between(t_aggr, c1, c2, color="grey", alpha=0.3)
+    plt.plot(t, c0)
+    plt.plot(t, c1, color="grey", alpha=0.3)
+    plt.plot(t, c2, color="grey", alpha=0.3)
+    plt.fill_between(t, c1, c2, color="grey", alpha=0.3)
 
-    plt.xlim(1, len(x1))    
+    plt.xlim(1, len(c0))    
     plt.ylim(0, 3)
     plt.title('Aggregation days = ' + str(n_aggr))
 
