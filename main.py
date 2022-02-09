0874faacc3dbe0eda49b200f46b45dacb8706b09
sadda <lukas.adam.cr@gmail.com>
2022-02-09 14:27:39 +0100

    Gif upgraded

--- main.py
+++ main.py
@@ -1,9 +1,15 @@
 import os
+import pandas
 import imageio
 from utilities import *
 
-t, x1, x2 = generate_data()
+data = pandas.read_csv(os.path.join('data', 'aggregated1.csv'))
+instagram = pandas.read_csv(os.path.join('data', 'aggregated2.csv'))
+
+year1 = 2019
+year2 = 2020
+place = None
 
 fps = 3
 kwargs_write = {'fps':fps, 'quantizer':'nq'}
-imageio.mimsave(os.path.join('results', 'aggr.gif'), [fig_to_matrix(plot(x1, x2, i)) for i in range(1,20)], fps=fps)
+imageio.mimsave(os.path.join('results', 'aggr.gif'), [fig_to_matrix(plot_confidence(data, year1, year2, i, instagram, place=place, return_fig=True)) for i in range(1,20)], fps=fps)
