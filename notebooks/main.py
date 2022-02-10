82f8ce9d012a6f1fef952dfb8258cc7854be90e3
sadda <lukas.adam.cr@gmail.com>
2022-02-10 16:59:22 +0100

    Gif regenerated

--- notebooks/main.py
+++ notebooks/main.py
@@ -12,7 +12,9 @@ instagram = pandas.read_csv(os.path.join('data', 'aggregated2.csv'))
 year1 = 2019
 year2 = 2020
 place = None
+ignore_shorter = True
+box_shape = True
 
 fps = 3
 kwargs_write = {'fps':fps, 'quantizer':'nq'}
-imageio.mimsave(os.path.join('figures', 'aggr.gif'), [fig_to_matrix(plot_confidence(data, year1, year2, i, instagram, place=place, return_fig=True)) for i in range(1,20)], fps=fps)
+imageio.mimsave(os.path.join('figures', 'aggr.gif'), [fig_to_matrix(plot_confidence(data, year1, year2, i, instagram, place=place, return_fig=True, ignore_shorter=ignore_shorter, box_shape=box_shape)) for i in range(1,20)], fps=fps)
