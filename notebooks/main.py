65a632040c1d5719b3810600e025cc2751663883
sadda <lukas.adam.cr@gmail.com>
2022-02-15 15:24:48 +0100

    Generate csv files

--- notebooks/main.py
+++ notebooks/main.py
@@ -35,3 +35,28 @@ v1s = np.power(10, np.linspace(1,4,100))
 fun = lambda rat: plot_k(v1s, v1s*rat, 1e-4, 1e-4, ymin=0, ymax = 2, xscale='log', title='Ratio of detected viewing pressures = ' + str(rat), return_fig=True)
 file_name = os.path.join('figures', 'confidence.gif')
 create_gif(fun, np.round(np.arange(0.1, 1.1, 0.1),1), file_name)
+
+
+
+'''
+from analysis import get_entries, prepare_data
+
+ignore_shorter=True
+box_shape=True
+
+for (year1, year2) in zip((2019, 2019), (2020, 2021)):
+    for place in ('boat', 'underwater'):
+        for n_aggr in (1, 7, 15, 184):
+            entries, years, months = get_entries(data, place=place)
+            c0, c1, c2, t = prepare_data(entries, years, year1, year2, months, min(months), max(months), n_aggr, instagram, ignore_shorter=ignore_shorter, box_shape=box_shape)
+            c0_no_inst, _, _, _ = prepare_data(entries, years, year1, year2, months, min(months), max(months), n_aggr, None, ignore_shorter=ignore_shorter, box_shape=box_shape)
+
+            df = pandas.DataFrame({'x': t, 'photos_lo': c1, 'photos_up': c2, 'photos_point': c0,
+                'arrivals_exact': np.full(len(t), np.nan),
+                'photos1': np.full(len(t), np.nan),
+                'photos2': np.full(len(t), np.nan),
+                'photos_point_initial': c0_no_inst})
+
+            file_name = "Ratio_%d_%d_%s_1_%02d_all.csv" % (year2,year1,place,n_aggr)
+            df.to_csv(file_name, index=None)
+'''
